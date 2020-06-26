6f3fb29ac55bb186e4f0c0efeabc7412b5d007f4
Matt Wilke <matt.wilke08@gmail.com>
2020-06-26 13:29:16 -0500

    Updates

---
+++ precache-manifest.4bf238c8af366f889c2c82c3d29928b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f921d7657fc9d03972d3237f9563175",
+    "url": "/mwilke08/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/mwilke08/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f890bfb908273ed0cf07",
+    "url": "/mwilke08/robofriends/static/css/main.379a709a.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/mwilke08/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mwilke08/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f890bfb908273ed0cf07",
+    "url": "/mwilke08/robofriends/static/js/main.b053766d.chunk.js"
+  },
+  {
+    "revision": "dbd51eda2ca94f58c9a9",
+    "url": "/mwilke08/robofriends/static/js/runtime-main.c41bca14.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/mwilke08/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
